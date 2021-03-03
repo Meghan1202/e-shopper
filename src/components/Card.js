0145c3eb47b63965b29a676a853da44f5d4ba25a
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-03-03 10:39:41 +0530

    feat(card-component): Add a card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './Card.css';
 
-class Card extends React.Component {
-    render() {
-        return (
-            <div className="card">
-                <img id='banana__img' src="https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg" alt="Banana"></img>
-                    <div className="container">
-                        <h4><b>Banana</b></h4>
-                        <p>One kg of Banana</p>
-                    </div>
-            </div> 
-        );
-    }
-} 
+function Card(props) {
+    return (
+        <div className="card">
+            <img id='banana__img' src="https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg" alt="Banana"></img>
+            <div className="container">
+                <h4><b>{this.props.value}</b></h4>
+                <p>One kg of {this.props.value}</p>
+            </div>
+        </div>
+    );
+}
 
 export default Card

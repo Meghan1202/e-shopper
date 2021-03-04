b2dd1a0602022858f9ed6f0ae2b09dda8cd97933
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-03-04 10:08:15 +0530

    feat: lift the state to App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [{
+        id: 1,
+        companyName: 'Fresho',
+        productName: 'Banana',
+        price: 40,
+        quantity: '1 kg',
+        count: 0,
+        imgSrc: 'assets/fruits/banana.png',
+      }, {
+        id: 2,
+        companyName: 'Fresho',
+        productName: 'Mango',
+        price: 40,
+        quantity: '1 kg',
+        count: 0,
+        imgSrc: 'assets/fruits/mango.png',
+      }, {
+        id: 3,
+        companyName: 'Fresho',
+        productName: 'Orange',
+        price: 40,
+        quantity: '1 kg',
+        count: 0,
+        imgSrc: 'assets/fruits/orange.png',
+      }, {
+        id: 4,
+        companyName: 'Fresho',
+        productName: 'Apple',
+        price: 40,
+        quantity: '1 kg',
+        count: 0,
+        imgSrc: 'assets/fruits/apple.png',
+      }, {
+        id: 5,
+        companyName: 'Fresho',
+        productName: 'Strawberry',
+        price: 40,
+        quantity: '1 kg',
+        count: 0,
+        imgSrc: 'assets/fruits/strawberry.png',
+      }],
+      cartCount: 0,
+    };
+  }
+
+  onIncrement = (id) => {
+    const { cartCount } = this.state;
+    const { products } = this.state;
+    const newState = {
+      ...this.state,
+      cartCount: cartCount + 1,
+      products: products.map((eachProduct) => {
+        if (eachProduct.id === id) {
+          return { ...eachProduct, count: eachProduct.count + 1 };
+        }
+        return eachProduct;
+      }),
+    };
+    this.setState(newState);
+  }
+
+  onDecrement = (product) => {
+    const { cartCount } = this.state;
+    if (product.count === 0) return;
+    const { products } = this.state;
+    const newState = {
+      ...this.state,
+      cartCount: cartCount - 1,
+      products: products.map((eachProduct) => {
+        if (eachProduct.id === product.id && eachProduct.count > 0) {
+          return { ...eachProduct, count: eachProduct.count - 1 };
+        }
+        return eachProduct;
+      }),
+    };
+    this.setState(newState);
+  }
+
+  render() {
+    const { products, cartCount } = this.state;
+    return (
+      <>
+        <NavBar items={cartCount} />
+        <h3> Fruits And Vegitables</h3>
+        <Home
+          products={products}
+          onDecrement={this.onDecrement}
+          onIncrement={this.onIncrement}
+        />
+      </>
+    );
+  }
 }
-
-export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,114 +1,27 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Product from './Product';
 import './Home.css';
-import NavBar from './NavBar';
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [{
-        id: 1,
-        companyName: 'Fresho',
-        productName: 'Banana',
-        price: 40,
-        quantity: '1 kg',
-        count: 0,
-        imgSrc: 'assets/fruits/banana.png',
-      }, {
-        id: 2,
-        companyName: 'Fresho',
-        productName: 'Mango',
-        price: 40,
-        quantity: '1 kg',
-        count: 0,
-        imgSrc: 'assets/fruits/mango.png',
-      }, {
-        id: 3,
-        companyName: 'Fresho',
-        productName: 'Orange',
-        price: 40,
-        quantity: '1 kg',
-        count: 0,
-        imgSrc: 'assets/fruits/orange.png',
-      }, {
-        id: 4,
-        companyName: 'Fresho',
-        productName: 'Apple',
-        price: 40,
-        quantity: '1 kg',
-        count: 0,
-        imgSrc: 'assets/fruits/apple.png',
-      }, {
-        id: 5,
-        companyName: 'Fresho',
-        productName: 'Strawberry',
-        price: 40,
-        quantity: '1 kg',
-        count: 0,
-        imgSrc: 'assets/fruits/strawberry.png',
-      }],
-      cartCount: 0,
-    };
-  }
+const Home = ({ products, onIncrement, onDecrement }) => (
+  <>
+    <div className="card__row">
+      {products.map((product) => (
+        <Product
+          key={product.id}
+          productId={product.id}
+          productName={product.productName}
+          productPrice={product.price}
+          productQuantity={product.quantity}
+          count={product.count}
+          companyName={product.companyName}
+          imgSrc={product.imgSrc}
+          onIncrement={() => onIncrement(product.id)}
+          onDecrement={() => onDecrement(product)}
+        />
+      ))}
+    </div>
+  </>
+);
 
-  onIncrement(id) {
-    const { cartCount } = this.state;
-    const { products } = this.state;
-    const newState = {
-      ...this.state,
-      cartCount: cartCount + 1,
-      products: products.map((eachProduct) => {
-        if (eachProduct.id === id) {
-          return { ...eachProduct, count: eachProduct.count + 1 };
-        }
-        return eachProduct;
-      }),
-    };
-    this.setState(newState);
-  }
-
-  onDecrement(product) {
-    const { cartCount } = this.state;
-    if (product.count === 0) return;
-    const { products } = this.state;
-    const newState = {
-      ...this.state,
-      cartCount: cartCount - 1,
-      products: products.map((eachProduct) => {
-        if (eachProduct.id === product.id && eachProduct.count > 0) {
-          return { ...eachProduct, count: eachProduct.count - 1 };
-        }
-        return eachProduct;
-      }),
-    };
-    this.setState(newState);
-  }
-
-  render() {
-    const { products } = this.state;
-    const { cartCount } = this.state;
-    return (
-      <>
-        <NavBar items={cartCount} />
-        <h3> Fruits And Vegitables</h3>
-        <div className="card__row">
-          {products.map((product) => (
-            <Product
-              key={product.id}
-              productId={product.id}
-              productName={product.productName}
-              productPrice={product.price}
-              productQuantity={product.quantity}
-              count={product.count}
-              companyName={product.companyName}
-              imgSrc={product.imgSrc}
-              onIncrement={() => { this.onIncrement(product.id); }}
-              onDecrement={() => { this.onDecrement(product); }}
-            />
-          ))}
-        </div>
-      </>
-    );
-  }
-}
+export default Home;

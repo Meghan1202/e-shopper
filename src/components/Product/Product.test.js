ded76c50146300750f9a836957a5e8e8675c033f
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-03-09 18:16:31 +0530

    improv: add test cases for Product

--- src/components/Product/Product.test.js
+++ src/components/Product/Product.test.js
@@ -22,4 +22,16 @@ describe(Product.name, () => {
     render(<Product productName="Banana" productPrice={40} productQuantity="2" onIncrement={jest.fn()} onDecrement={jest.fn()} count={1} companyName="Fresho" imgSrc="abc" />);
     screen.getByTestId('quantity-count');
   });
+  test('should display the image of the product', () => {
+    render(<Product productName="Banana" productPrice={40} productQuantity="2" onIncrement={jest.fn()} onDecrement={jest.fn()} count={1} companyName="Fresho" imgSrc="abc" />);
+    screen.getByTestId('product-image');
+  });
+  test('should display the quantity of products', () => {
+    render(<Product productName="Banana" productPrice={40} productQuantity="2" onIncrement={jest.fn()} onDecrement={jest.fn()} count={1} companyName="Fresho" imgSrc="abc" />);
+    screen.getByText('Quantity:2');
+  });
+  test('should display the price of the products', () => {
+    render(<Product productName="Banana" productPrice={40} productQuantity="2" onIncrement={jest.fn()} onDecrement={jest.fn()} count={1} companyName="Fresho" imgSrc="abc" />);
+    screen.getByText('40/-');
+  });
 });

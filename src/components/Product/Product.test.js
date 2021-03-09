f583947580674e68e2ccaabd07434f77d99629d8
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-03-09 18:17:17 +0530

    refactor: fix test cases for Product

--- src/components/Product/Product.test.js
+++ src/components/Product/Product.test.js
@@ -1,6 +1,4 @@
-import {
-  fireEvent, render, screen, waitFor,
-} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Product from './Product';
 
